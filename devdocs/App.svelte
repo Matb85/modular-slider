fcbe88bf355c0d843ef497bf48b20246eb53a55a
Mateusz BIS <Mateuszbis85@gmail.com>
2021-03-23 09:57:50 +0100

    docs: :mag: add meta tags

--- devdocs/App.svelte
+++ devdocs/App.svelte
@@ -11,12 +11,12 @@
 
 <svg xmlns="http://www.w3.org/2000/svg" class="fixed top-full w-0 h-0">
   <filter id="aurora">
-    <feTurbulence baseFrequency="0.0007" seed="14" type="fractalNoise" numOctaves="2"></feTurbulence>
-    <feColorMatrix type="saturate" values="10" in="blur" result="colormatrix"></feColorMatrix>
+    <feTurbulence baseFrequency="0.0007" seed="49" type="fractalNoise" numOctaves="2"></feTurbulence>
+    <feColorMatrix type="saturate" values="10" result="colormatrix"></feColorMatrix>
     <feColorMatrix type="hueRotate" values="90" in="colormatrix" result="colormatrix1"></feColorMatrix>
     <feGaussianBlur stdDeviation="30" in="turbulence" edgeMode="none" result="blur"></feGaussianBlur>
     <feComponentTransfer>
-      <feFuncA type="linear" slope="0.6"></feFuncA>
+      <feFuncA type="linear" slope="0.5"></feFuncA>
     </feComponentTransfer>
   </filter>
 </svg>
