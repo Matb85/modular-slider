69e1dd37b6d60582733796069eb41f7a9f797a03
matb85 <mateuszbis85@gmail.com>
2022-04-29 21:36:53 +0200

    docs: :memo: update docs

--- devdocs/components/sliders/Basic.svelte
+++ devdocs/components/sliders/Basic.svelte
@@ -6,17 +6,24 @@
     {/each}
   </div>
 </section>
+<section class="flex justify-center gap-4 mt-4">
+  <button id="prev" on:click="{() => sl.slidePrev()}" class="button">slider.slidePrev()</button>
+  <button id="next" on:click="{() => sl.slideNext()}" class="button">slider.slideNext()</button>
+  <button id="next" on:click="{() => sl.slideTo(1)}" class="button">slider.slideTo(1)</button>
+  <button id="next" on:click="{() => sl.slideBy(2)}" class="button">slider.slideBy(2)</button>
+  <button id="next" on:click="{() => sl.slideBy(-2)}" class="button">slider.slideBy(-2)</button>
+</section>
 <SliderCode>{code}</SliderCode>
 
-<script lang="ts">
+<script>
 import SliderCode from "~/components/SliderCode.svelte";
 import { onMount } from "svelte";
 import Slider from "../factories/noloopFactory";
 
 const sliderItems = [0, 1, 2, 3, 4, 5, 6];
-
+let sl;
 onMount(async () => {
-  new Slider({
+  sl = new Slider({
     container: "basic-slider",
     transitionSpeed: 400,
   });
