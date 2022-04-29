6280ef0cadcb80482446830559e2d3cb5fa7eaca
matb85 <mateuszbis85@gmail.com>
2022-04-29 18:29:33 +0200

    feat: :construction_worker: use vite for bundling

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,31 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 const dedupe = ["svelte"];
-export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    dedupe,
-    alias: {
-      "@": process.cwd() + "/src",
-      "~": process.cwd() + "/devdocs",
-    },
-  },
-  root: "./devdocs",
-  build: {
-    minify: true,
-    outDir: "../docs",
-    emptyOutDir: true,
-  },
+const name = "modular-slider";
+const entry = "./src/index.ts";
+const alias = {
+  "@": process.cwd() + "/src",
+  "~": process.cwd() + "/devdocs",
+};
+
+export default defineConfig(() => {
+  if (process.env.TARGET == "DOCS")
+    return {
+      plugins: [svelte()],
+      resolve: { dedupe, alias },
+      root: "./devdocs",
+      build: {
+        minify: true,
+        outDir: "../docs",
+        emptyOutDir: true,
+      },
+    };
+  else
+    return {
+      resolve: { dedupe, alias },
+      build: {
+        lib: { entry, name, formats: ["es"], fileName: format => name + "." + format + ".mjs" },
+        outDir: "dist",
+      },
+    };
 });
